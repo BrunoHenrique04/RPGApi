d6b60a87669b62d5a5eb6f2f099ac28973dd08e8
Bruno <brunohmfcorporativo@gmail.com>
2024-04-02 22:50:33 -0300

    feat: add publish

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(Options => 
 {
-    Options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoLocal"));
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoSomee"));
 });
 
 // Add services to the container.
